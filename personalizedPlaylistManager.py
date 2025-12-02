1cdecef7c2378474764f8f47a615d0ca2ee78da7
Muhammad Asad Ullah <me.asadullah9@gmail.com>
2025-12-02 15:58:10 +0500

    Implement personalized playlist manager functionality

---
+++ personalizedPlaylistManager.py
@@ -0,0 +1,36 @@
+playlist = {}
+while True:
+    print('\n---- Playlist Manager ----')
+    print("1. Add Song")
+    print("2. Remove Song")
+    print("3. View Playlist")
+    print("4. Search Song")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: ").strip())
+    if choice == 1:
+        title = input("Enter your Song's Title: ").strip()
+        artist = input("Enter your Song's Artist: ").strip()
+        if title in playlist:
+            print("Song already exists")
+        else:
+            playlist[title] = artist
+            print("Song added")
+    elif choice == 2:
+        title = input("Enter your Song's Title: ").strip()
+        if title not in playlist:
+            print("Song does not exist")
+        else:
+            del playlist[title]
+            print(f"Song removed: {title}")
+    elif choice == 3:
+        if playlist:
+            print("\nYour Playlist")
+            for title, artist in playlist.items():
+                print(f"Title: {title}, Artist: {artist}")
+    elif choice == 4:
+        title = input("Enter your Song's Title: ").strip()
+        if title in playlist:
+            print(f"{title} is in your playlist, sung by {playlist[title]}.")
+    if choice == 5:
+        break
